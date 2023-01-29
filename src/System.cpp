23757e0db1c2a3efd646badf6e24ff1a3835ee6b
Moss <moss@mossx.net>
2023-01-28 16:17:59 -0800

    Changed Workspace Symbols Vector to be Created by Size

--- src/System.cpp
+++ src/System.cpp
@@ -28,7 +28,7 @@ namespace System
         std::string lockCommand = ""; // idk, no standard way of doing this.
         std::string exitCommand = ""; // idk, no standard way of doing this.
         std::string batteryFolder = ""; // this can be BAT0, BAT1, etc. Usually in /sys/class/power_supply
-        std::vector<std::string> workspaceSymbols = {"", "", "", "", "", "", "", "", ""};
+        std::vector<std::string> workspaceSymbols = std::vector<std::string>(9, "");
         std::string defaultWorkspaceSymbol = "";
     };
 
