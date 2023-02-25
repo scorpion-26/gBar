8e6f766cb334e4980bc441c9fd90d57b431bf342
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-02-25 20:20:25 +0100

    stuff

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     };
   };
 
-  outputs = { nixpkgs, flake-utils, stdenv, hyprland, ... }: flake-utils.lib.eachDefaultSystem (system:
+  outputs = { self, nixpkgs, flake-utils, hyprland, ... }: flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
@@ -48,6 +48,7 @@
         ];
       };
     }
+
   );
 }
 
