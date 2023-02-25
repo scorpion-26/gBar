a51caefd31a054391a392ad01f416b7a2366e0ae
Edvin Källström <edvin.kallstrom@protonmail.com>
2023-02-25 21:30:02 +0100

    quickfix

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
     flake-utils.url = "github:numtide/flake-utils";
   };
 
-  outputs = { self, nixpkgs, flake-utils, hyprland, ... }: flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+  outputs = { self, nixpkgs, flake-utils, ... }: flake-utils.lib.eachSystem ["x86_64-linux"] (system:
     let
       pkgs = import nixpkgs {
         inherit system;
