80ade2b06e6831c7390036765268f1a49cbe0906
scorpion-26 <58082714+scorpion-26@users.noreply.github.com>
2023-05-05 23:13:35 +0200

    Fix inconsistent whitespace

--- src/AudioFlyin.cpp
+++ src/AudioFlyin.cpp
@@ -51,7 +51,7 @@ namespace AudioFlyin
                     }
                     else
                     {
-                        icon->SetText("󰕾 ");
+                        icon->SetText("󰕾");
                     }
                 }
             }
